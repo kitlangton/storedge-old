892a0d46e82ab769cd99030e9629524b932cded9
sudokill <kit.langton@gmail.com>
2015-06-29 10:35:57 -0400

    Added search bar element.

---
+++ spec/features/user_can_search_for_products_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "User can search for products" do
+  scenario "user searches for a product", js: true do
+    user = create(:user)
+    company = user.company
+    folder = create(:folder, company: company)
+    product = create(:product, folder: folder, company: company)
+
+    login(user)
+    visit root_path
+
+    fill_in "product-search-field", with: product.name
+    find("#product-search-field").native.send_key(:Enter)
+
+    expect(page).to have_content product.name
+  end
+end
